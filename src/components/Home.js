2994cbfce4e777b8ef265366f7dfd34e54f19b5d
JOFIMOL JOHNSON <jofimolj@gmail.com>
2023-03-30 17:44:30 +0530

    last commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,14 @@ import { useState, useEffect } from "react";
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import Card from "react-bootstrap/Card";
-import "./Home.css";
+import { Row, Col } from "react-bootstrap";
 
 function Home() {
-  const navigate = useNavigate()
-  const handleLogout = () =>{
-    localStorage.removeItem("loggedin")
-    navigate("/sign-in")
-  }
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        localStorage.removeItem("loggedin");
+        navigate("/sign-in");
+    };
     const url = "https://restcountries.com/v2/all?fields=name,region,flag";
     const [data, setData] = useState([]);
 
@@ -25,26 +25,32 @@ function Home() {
     }, []);
 
     return (
-      
         <div className="App">
             <div>
-            <Button style={{float: "right",marginTop:"10px"}}  variant="dark" onClick={handleLogout}>LOGOUT</Button>
-
+                <Button style={{ float: "right", marginTop: "10px" }} variant="dark" onClick={handleLogout}>
+                    LOGOUT
+                </Button>
             </div>
-           <br/><br/><br/>
-            <center>
-                {data.map((dataObj, index) => {
-                    return (
-                        <Card style={{ width: "18rem" }}>
-                            <Card.Img variant="top" src={dataObj.flag} />
-                            <Card.Body>
-                                <Card.Title>{dataObj.name}</Card.Title>
-                                <Card.Text>Region:{dataObj.region}</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    );
-                })}
-            </center>
+            <br />
+            <br />
+            <br />
+            <Row className="row" sm={12} md={8} lg={6} xl={3}  >
+                    {data.map((dataObj, index) => {
+                        return (
+                           
+                                <Card style={{ width: "20rem" }}  className="my-3 mx-5 p-3">
+                                    <Card.Img style={{height:"200px"}} variant="top" src={dataObj.flag} />
+                                    <Card.Body>
+                                        <Card.Title>{dataObj.name}</Card.Title>
+                                        <Card.Text>Region:{dataObj.region}</Card.Text>
+                                    </Card.Body>
+                                </Card>
+                
+                        );
+                    })}
+            </Row>
+
+            
         </div>
     );
 }
